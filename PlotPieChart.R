bb182e9d2eb7a34e63aa5db85218ac99b3f39ade
Watchara Kesdech <kwatchara21@gmail.com>
2021-08-19 13:51:58 +0700

    Add histrogram

--- PlotPieChart.R
+++ PlotPieChart.R
@@ -5,3 +5,10 @@ cars <- c(1, 3, 6, 4, 9)
 # custom colors and labels
 pie(cars, main="Cars", col=rainbow(length(cars)),
    labels=c("Mon","Tue","Wed","Thu","Fri"))
+
+
+
+# Get a random log-normal distribution
+r <- rlnorm(1000)
+
+hist(r)
